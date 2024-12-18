0756f12cef2ebc13b4cfa2a5e631fe0eca2741e9
20221136@uthh.edu.mx <20221136@uthh.edu.mx>
2024-12-18 04:07:05 -0600

    Update login.component.ts

--- src/app/modules/public/pages/login/login.component.ts
+++ src/app/modules/public/pages/login/login.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../../../../shared/services/auth.service';
 import { ERol } from '../../../../shared/constants/rol.enum';
+import { AlertTaiwilService } from '../../../../shared/services/alert-taiwil.service';
 
 @Component({
   selector: 'app-login',
@@ -15,7 +16,8 @@ export class LoginComponent {
 
   constructor(
     private fb: FormBuilder,
-    private router: Router,
+    private router: Router,    private alertTaiwilService: AlertTaiwilService,
+    
     private authService: AuthService
   ) {
     this.loginForm = this.fb.group({
@@ -28,37 +30,48 @@ export class LoginComponent {
     if (this.loginForm.valid) {
       this.loading = true;
       const { email, password } = this.loginForm.value;
-
+  
       this.authService.login(email, password).subscribe(
         async (response) => {
           const { token } = response;
-
+  
           if (token) {
             await this.authService.setToken(token);
             const rol = await this.authService.getRoleFromToken();
-
+  
             if (rol) {
               console.log('Rol obtenido:', rol);
-              alert(`Inicio de sesión exitoso con rol: ${rol}`);
-              this.redirectByRole(rol as ERol);
+  
+              // Verificamos si el rol es PENDIENTE
+              if (rol === 'PENDIENTE') {
+                // Mostrar alerta específica si el rol es PENDIENTE
+                this.alertTaiwilService.showTailwindAlert(
+                  'Tu rol está pendiente. No tienes acceso al sistema. Por favor, ponte en contacto con soporte.',
+                  'warning'
+                );
+              } else {
+                alert(`Inicio de sesión exitoso con rol: ${rol}`);
+                this.redirectByRole(rol as ERol);
+              }
             } else {
               alert('No se pudo determinar el rol.');
             }
           }
-
+  
           this.loading = false;
         },
         (error) => {
           this.loading = false;
           console.error('Error en inicio de sesión:', error);
-          alert('Credenciales incorrectas');
+          // alert('Credenciales incorrectas');
+          this.alertTaiwilService.showTailwindAlert('Credenciales incorrectas', 'error');
         }
       );
     } else {
       alert('Por favor completa el formulario correctamente.');
     }
   }
-
+  
   redirectByRole(rol: ERol) {
     switch (rol) {
       case ERol.ADMIN:
@@ -77,7 +90,7 @@ export class LoginComponent {
         this.router.navigate(['docente/']);
         break;
       case ERol.ALUMNO:
-        this.router.navigate(['alumno/home']);
+        this.router.navigate(['alumno/']);
         break;
       case ERol.CONTROL_ESCOLAR:
         this.router.navigate(['control/']);
