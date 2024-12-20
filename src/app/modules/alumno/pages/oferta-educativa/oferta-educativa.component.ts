e0d7de09c865f8f5272193ad2b21da67e1c1f7cc
20221136@uthh.edu.mx <20221136@uthh.edu.mx>
2024-12-20 06:05:40 -0600

    Add oferta-educativa.component.ts

---
+++ src/app/modules/alumno/pages/oferta-educativa/oferta-educativa.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-oferta-educativa',
+  templateUrl: './oferta-educativa.component.html',
+  styleUrl: './oferta-educativa.component.scss'
+})
+export class OfertaEducativaComponent {
+
+}
