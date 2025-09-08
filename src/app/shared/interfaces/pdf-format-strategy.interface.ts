9b410cd52959906e45cedadaccb87ea7e269f629
diaz-rigo <morelosalfaro@gmail.com>
2025-09-08 00:07:27 -0600

    dede

--- src/app/shared/interfaces/pdf-format-strategy.interface.ts
+++
@@ -1,8 +0,0 @@
-import jsPDF from 'jspdf';
-import { CursoPdfData } from '../types/curso-pdf-data.type';
-import { PdfHelpers } from '../helpers/pdf-helpers';
-
-
-export interface PdfFormatStrategy {
-  generate(doc: jsPDF, data: CursoPdfData, helpers: PdfHelpers): void;
-}
--- src/app/shared/pipes/validators.ts
+++
@@ -1,9 +0,0 @@
-import { AbstractControl, ValidationErrors, ValidatorFn } from "@angular/forms";
-
-export function emailValidator(): ValidatorFn {
-  return (control: AbstractControl): ValidationErrors | null => {
-    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}(?<![.@])$/;
-    const valid = emailRegex.test(control.value);
-    return valid ? null : { invalidEmail: true };
-  };
-}
